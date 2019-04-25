faf35ca68d946df11c7f1345dd8ee864a40ff593
Ubuntu <ubuntu@ip-172-31-26-231.us-west-1.compute.internal>
2019-04-25 08:32:57 +0000

    Switch to python 3.5

--- health/urls.py
+++ health/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.health_check, name='health_check'),
-]
+    path('', views.health_check, name='health_check'),
+]          
